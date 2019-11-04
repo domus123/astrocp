a6ea9f19a8d9bfd07eb35988256d926deb5baa92
Lucas Guerra <lu.guerra7508@hotmail.com>
2019-11-03 23:16:41 -0300

    script for running multiple servers

---
+++ src_python/server/run_servers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import os
+import sys 
+
+PORT = 52000
+try:
+        N = int(sys.argv[1])
+except:
+        N = 3
+        
+print(f"N {N}")
+for i in range(N):
+        print(f"PORT: {PORT}")
+        build_x = f'docker build -t astro_server{i} .'
+        run_x = f'docker run -d -p {PORT}:{PORT} --env GRPC_PORT={PORT} --env MAP_ID={i + 1} --network="host" astro_server{i}'
+        print(f"Running: {build_x}")
+        os.system(build_x)
+        print(f"Running: {run_x}")
+        os.system(run_x)
+        PORT += 2
+	
+        
+
